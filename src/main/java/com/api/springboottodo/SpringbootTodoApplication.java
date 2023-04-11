283182cae92049e21f2246fe8501770edc0aef17
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2023-04-11 10:08:54 +0200

    initialized app

---
+++ src/main/java/com/api/springboottodo/SpringbootTodoApplication.java
@@ -0,0 +1,13 @@
+package com.api.springboottodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTodoApplication.class, args);
+	}
+    
+}
